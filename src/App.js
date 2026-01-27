9744541c7ddc401b9006956ac5a9d1d64d6afea5
GastonCuevas <gaston.cuevas@infocorpgroup.com>
2026-01-27 11:59:47 -0300

    Add(): lista de autos

--- src/App.js
+++ src/App.js
@@ -1,64 +1,39 @@
 import React, { Component } from 'react';
+import cars from './data/cars.json';
 import logo from './logo.svg';
 import './App.css';
 
+class CarItem extends Component {
+  render() {
+    const { car, id } = this.props;
+    return (
+      <li>
+        <p><strong>Key: </strong>{id}</p>
+        <p><strong>Nombre: </strong>{car.name}</p>
+        <p><strong>Marca: </strong>{car.company}</p>
+      </li>
+    );
+  }
+}
+
 class App extends Component {
   render() {
+    // const numbers = [1, 1, 3, 4, 5];
+
     return (
       <div className="App">
-        <header className="App-header">
-          <div style={{ 
-            display: 'grid', 
-            gridTemplateColumns: '1fr 1fr', 
-            gap: '40px',
-            alignItems: 'center',
-            justifyItems: 'center',
-            maxWidth: '1200px',
-            margin: '0 auto'
-          }}>
-            <div>
-              <img src={logo} className="App-logo" alt="logo" />
-              <h1>Hola mundo!</h1>
-              <h3>Vamos a aprender React</h3>
-              <p>Hola mundo, estoy aprendiendo React</p>
-              <strong>Y lo estoy disfrutando</strong>
-            </div>
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <img src='https://animesweet.com/wp-content/uploads/2022/12/kimetsu-no-yaiba-1-scaled.jpg' alt='KNYLogo' style={{ 
-                width: '500px',
-                height: '300px'
-              }}></img>
-              <table style={{ 
-                backgroundColor: 'white', 
-                borderRadius: '10px', 
-                padding: '20px', 
-                marginTop: '20px', 
-                color: 'black',
-                borderCollapse: 'collapse',
-                width: '300px'
-              }}>
-                <thead>Kimetsu No Yaiba Characters</thead>
-                <tbody>
-                  <tr style={{ borderBottom: '1px solid #ddd' }}>
-                    <td style={{ padding: '12px' }}>Tanjiro Kamado</td>
-                  </tr>
-                  <tr style={{ borderBottom: '1px solid #ddd' }}>
-                    <td style={{ padding: '12px' }}>Nezuko Kamado</td>
-                  </tr>
-                  <tr style={{ borderBottom: '1px solid #ddd' }}>
-                    <td style={{ padding: '12px' }}>Zenitsu Agatsuma</td>
-                  </tr>
-                  <tr style={{ borderBottom: '1px solid #ddd' }}>
-                    <td style={{ padding: '12px' }}>Inosuke Hashibira</td>
-                  </tr>
-                  <tr>
-                    <td style={{ padding: '12px' }}>Kanao Tsuyuri</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </header>
+        {/* <h4>Trabajando con listas</h4> */}
+        {/* {numbers.map((number, index) => (
+          <p key={number}>El número es: {number}</p>
+        ))} */}
+        <h4>Trabajando con listas de objetos</h4>
+        <ul>
+          {cars.map((car) => {
+            return (
+              <CarItem key={car.id} id={car.id} car={car} />
+            );
+          })}
+        </ul>
       </div>
     );
   }  
